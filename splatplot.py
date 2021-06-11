d40de1e364d9e18f8fb03d9f53240717dc67f4d2
Yottaphy <jorge13romero@gmail.com>
2021-06-11 17:06:59 +0300

    added chamfer dependency and results for 1 cham

--- splatplot.py
+++ splatplot.py
@@ -2,24 +2,29 @@ import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 
-def top(x):
-    if x <= -109.5:
+def top(x, cham):
+    if x <= -109.5 and cham ==2:
         return -x-220
     elif x<= -104:
         return -110.5
     else:
         return x-6.5
 
-def bottom(x):
-    if x <= -109.5:
+def bottom(x, cham):
+    if x <= -109.5 and cham ==2:
         return x-6
     elif x<= -104:
         return -115.5
     else:
         return -x-219.5
 
-n,t,x,y,z,k = np.genfromtxt( "splats.txt", unpack=True, skip_header=87, skip_footer=1, autostrip=True)
+#expected txt file: ion number, tof, x, y, z, kinetic energy
+n,t,x,y,z,k = np.genfromtxt( "splats1cham.txt", unpack=True, skip_header=87, skip_footer=1, autostrip=True)
 
+cham = 1  #number of chamfers
+
+
+#compute the positions of splats that don't reach the end
 lostx = []
 losty = []
 lostz = []
@@ -28,7 +33,8 @@ for i in range(len(z)):
         lostx.append(x[i])
         losty.append(y[i])
         lostz.append(z[i])
-        
+
+#draw the aperture cross-section        
 xtop = []
 xbot = []
 ytop = []
@@ -36,11 +42,11 @@ ybot = []
 zplot = np.arange(-112,-101.51, 0.01)
 
 for i in range(len(zplot)):
-    xtop.append(top(zplot[i])+113)
-    xbot.append(bottom(zplot[i])+113)
+    xtop.append(top(zplot[i], cham)+113)
+    xbot.append(bottom(zplot[i], cham)+113)
 
-    ytop.append(top(zplot[i]))
-    ybot.append(bottom(zplot[i]))
+    ytop.append(top(zplot[i], cham))
+    ybot.append(bottom(zplot[i], cham))
 
 radius = 2.5
 big    = 4.5
@@ -57,6 +63,9 @@ az = axes[2]
 
 ax.plot(zplot, xtop, color = eleccol)
 ax.plot(zplot, xbot, color = eleccol)
+if cham == 1:
+    ax.axvline(x=min(zplot),ymin=min(xtop),ymax=max(xtop), color=eleccol)
+    ax.axvline(x=min(zplot),ymin=min(xbot),ymax=max(xbot), color=eleccol)
 ax.plot(lostz, lostx, 'o', color = pointcol)
 ax.set_title("Y Slice")  
 ax.set_xlabel("z [mm]")
@@ -65,6 +74,9 @@ ax.set_ylim(-5,5)
 
 ay.plot(zplot, ytop, color = eleccol)
 ay.plot(zplot, ybot, color = eleccol)
+if cham == 1:
+    ay.axvline(x=min(zplot),ymin=min(ytop),ymax=max(ytop), color=eleccol)
+    ay.axvline(x=min(zplot),ymin=min(ybot),ymax=max(ybot), color=eleccol)
 ay.plot(lostz, losty, 'o', color = pointcol)
 ay.set_title("X Slice")  
 ay.set_xlabel("z [mm]")
@@ -84,5 +96,5 @@ for i in range(0,3):
     axes[i].tick_params(axis="y",direction="in", right="on",labelleft="on")
 
 fig.tight_layout()
-fig.savefig("splats.pdf", bbox_inches = 'tight', pad_inches = 0.1, transparent=True)
-fig.savefig("splats.png", bbox_inches = 'tight', pad_inches = 0.1, transparent=True)
+fig.savefig("splats"+str(cham)+"cham.pdf", bbox_inches = 'tight', pad_inches = 0.1, transparent=True)
+fig.savefig("splats"+str(cham)+"cham.png", bbox_inches = 'tight', pad_inches = 0.1, transparent=True)
